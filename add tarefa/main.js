ee51813e1836cea21b17049c659b8e8c97337719
JaumF <netjoaof1@gmail.com>
2024-08-28 10:58:17 -0300

    novo

---
+++ add tarefa/main.js
@@ -0,0 +1,11 @@
+// Selecionado os elementos do HTML
+const InputTarefa = document.getElementById('tarefa')
+const btnAdicionar = document.getElementById('adicionar')
+const listaTarefas = document.getElementById('tarefas')
+
+document.addEventListener('click', adicionarTarefa);
+
+// Função para adicionar uma nova tarefa
+function adicionarTarefa() {
+    alert("Entrou na função");
+}
