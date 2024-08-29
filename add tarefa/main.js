069ccdd30a2d29ec1f8f89184ab3800401462a8d
JaumF <netjoaof1@gmail.com>
2024-08-29 08:44:54 -0300

    novo

--- add tarefa/main.js
+++ add tarefa/main.js
@@ -7,5 +7,20 @@ btnAdicionar.addEventListener('click', adicionarTarefa);
 
 // Função para adicionar uma nova tarefa
 function adicionarTarefa() {
-    alert("Entrou na função");
+    // Obter o valor que tem no campo tarefa
+    const textoTarefa = tarefa.value;
+
+    // Podemos utilizar o trim() para retirar os espaços desnecessários
+    if(textoTarefa.trim() !== ""){
+
+        // Criar um elemente li para inserir a tarefa na lista ul
+        const novaTarefa = document.createElement("li");
+        novaTarefa.innerHTML = textoTarefa;
+
+        // Acrescentar a tarefa no elemente pai ul
+        listaTarefas.appendChild(novaTarefa);
+        
+        // Limpar o campo de entrada (inpput)
+        tarefa.value = "";
+    }
 }
