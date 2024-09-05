285438e10a5698c9a2b50f6d2bb54d99960ad9cd
JaumF <netjoaof1@gmail.com>
2024-09-05 11:00:53 -0300

    Tudo pronto 05/09/2024

--- api/main.js
+++ api/main.js
@@ -13,3 +13,22 @@ button.addEventListener("click", ()=>{
     if(!input.value) return;
     obterDadosClima();
 } )
+
+async function obterDadosClima(){
+    let urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input.value)}&units=metric&appid=f2a12954671f8507cdb03aeb847faacf`;
+
+    try{
+        const resposta = await fetch(urlApi);
+        const dados = await resposta.json();
+        carregarInfoClima(dados);
+
+    }catch(error){
+        alert(error);
+    }
+}
+
+function carregarInfoClima(dados){
+    city.innerHTML = `${dados.name}, ${dados.sys.country}`;
+    degree.innerHTML = `Temperatura: ${Math.floor(dados.main.temp)} °C`;
+    img.src = `http://openweathermap.org/img/wn/${dados.weather[0].icon}@2x.png`;
+}
